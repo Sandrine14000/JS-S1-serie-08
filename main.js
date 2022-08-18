de8ca29b81f066c23b63a4fb7e4a0725613acd5a
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-08-18 11:33:18 +0200

    up 11h33

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let stagiaire1 = ['Martin', 'DUPONT'];
 let stagiaire2 = ['Vincent', 'MARTIN'];
 let stagiaires = [stagiaire1, stagiaire2];
 
-console.log (stagiaires[1][0]);
+console.log(stagiaires[1][0]);
 
 /**
  * 	Est-ce que ce que l’écriture permet de savoir facilement ce que l’on affiche ?
@@ -19,8 +19,15 @@ stagiaireA.prenom = "Martin";
 let stagiaireB = new Stagiaire();
 stagiaireB.nom = "MARTIN";
 stagiaireB.prenom = "Vincent";
-stagiaires=[stagiaireA, stagiaireB];
+
+stagiaires = [stagiaireA, stagiaireB];
+
 console.log(stagiaires);
-console.log (stagiaires[1].prenom);
+
+console.log(stagiaires[1].prenom);
 
 console.log(stagiaires[0].salutation());
+
+
+let anotherStagiaire = new Stagiaire("Lucette", "Anderson");
+console.log(anotherStagiaire);
--- stagiaire.js
+++ stagiaire.js
@@ -1,12 +1,15 @@
-class Stagiaire
-{
+class Stagiaire {
     nom;
     prenom;
 
 
-salutation (){
-    
-    return `je m'appelle ${this.prenom} ${this.nom}`;
-   // return "je m'appelle "+this.prenom+ " "+this.nom;
-}
+    salutation() {
+
+        return `je m'appelle ${this.prenom} ${this.nom}`;
+        // return "je m'appelle "+this.prenom+ " "+this.nom;
+    }
+    constructor(prenom, nom="") {
+        this.prenom = prenom.toLowerCase();
+        this.nom = nom.toUpperCase();
+    }
 }
